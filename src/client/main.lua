774fdde1a004d1b095ec5090da181dfce81bbaff
sei11041959 <sei1104.idea@gmail.com>
2022-08-29 15:32:28 +0900

    バックグラウンドの追加と多角形の描画！

--- src/client/main.lua
+++ src/client/main.lua
@@ -6,6 +6,8 @@ require("player")
 function love.load()
 	world = wf.newWorld(0, 0)
     world:addCollisionClass('Player')
+    world:addCollisionClass('Ground')
+    world:addCollisionClass('Platform')
     world:setCallbacks(beginContact,endContact)
     map:load()
 	Player:load()
@@ -17,11 +19,12 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.draw(love.graphics.newImage("assets/img/background.png"))
     map:draw()
     world:draw()
-	love.graphics.push()
+	--love.graphics.push()
 	Player:draw()
-	love.graphics.pop()
+	--love.graphics.pop()
 end
 
 function love.keypressed(key)
--- src/client/menu/map.lua
+++ src/client/menu/map.lua
@@ -2,14 +2,37 @@ local sti = require("lib.sti")
 
 map = {}
 collider = {}
+test = {}
 
 function map:load()
     MenuMap = sti("assets/maps/menu.lua", {"box2d"})
-    if MenuMap.layers["NormalCollied"] then
-        MenuMap.layers.NormalCollied.visible = false
-        for i, obj in ipairs(MenuMap.layers["NormalCollied"].objects) do
-            local collide = world:newRectangleCollider(obj.x,obj.y,obj.width,obj.height)
+    if MenuMap.layers["GroundCollision"] then
+        MenuMap.layers.GroundCollision.visible = false
+        for i, obj in ipairs(MenuMap.layers["GroundCollision"].objects) do
+            vertices = {}
+            for _, vertice in pairs(obj.polygon) do
+                table.insert(vertices,vertice.x)
+                table.insert(vertices,vertice.y)
+                table.insert(test,{x = vertice.x,y = vertice.y})
+            end
+            local collide = world:newChainCollider(true,vertices)
             collide:setType("static")
+            collide:setCollisionClass('Ground')
+            table.insert(collider,collide)--]]
+        end
+    end
+    if MenuMap.layers["PlatformCollision"] then
+        MenuMap.layers.PlatformCollision.visible = false
+        for i, obj in ipairs(MenuMap.layers["PlatformCollision"].objects) do
+            local vertices = {}
+            for _, vertice in pairs(obj.polygon) do
+                table.insert(vertices,vertice.x)
+                table.insert(vertices,vertice.y)
+                table.insert(test,{x = vertice.x,y = vertice.y})
+            end
+            local collide = world:newChainCollider(true,vertices)
+            collide:setType("static")
+            collide:setCollisionClass('Platform')
             table.insert(collider,collide)
         end
     end
@@ -17,10 +40,12 @@ end
 
 
 function map:update(dt)
-    
 end
 
 
 function map:draw()
     MenuMap:draw(0,0)
+    for index, v in ipairs(test) do
+        love.graphics.rectangle("fill",v.x-2.5,v.y-2.5,5,5)
+    end
 end
